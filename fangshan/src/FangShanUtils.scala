adb113cf7b1db63adfaa5d6b271d144aa232a2c8
Qiming Chu <cchuqiming@gmail.com>
2025-01-21 13:48:13 +0800

    [fangshan] Move withClockAndReset(connectClockAndReset) and dontCareInputs to FangShanUtils

---
+++ fangshan/src/FangShanUtils.scala
@@ -0,0 +1,31 @@
+package fangshan.utils
+
+import chisel3._
+
+import scala.collection.immutable.SeqMap
+
+object FangShanUtils {
+
+  /** Connect clock and reset to elements */
+  def withClockAndReset(element: SeqMap[String, Data], clock: Clock, reset: Reset): Iterable[Unit] = {
+    element.map { case (name, element) =>
+      name match {
+        case "clock" => element.asInstanceOf[Clock] := clock
+        case "reset" => element.asInstanceOf[Reset] := reset
+        case _       => ()
+      }
+    }
+  }
+
+  /** Set all inputs to DontCare except for ignorePorts */
+  def dontCareInputs(element: SeqMap[String, Data], ignorePorts: Seq[String]): Iterable[Unit] = {
+    ignorePorts.map { s =>
+      element.map { case (name, element) =>
+        name match {
+          case s => element := DontCare
+          case _ => ()
+        }
+      }
+    }
+  }
+}
