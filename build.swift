4be9ecccc70e2b15f47ad23e0b9b6718e18e391d
Qiming Chu <cchuqiming@gmail.com>
2025-01-25 18:37:43 +0800

    [script] Use swift scripts instead of Justfile

---
+++ build.swift
@@ -0,0 +1,119 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+func createShell(_ command: String) throws -> String {
+  let task = Process()
+
+  let pipe = Pipe()
+
+  task.standardOutput = pipe
+  task.standardError = pipe
+  task.arguments = ["-c", command]
+  task.executableURL = URL(fileURLWithPath: "/bin/zsh")
+  task.standardInput = nil
+
+  try task.run()
+
+  let data = pipe.fileHandleForReading.readDataToEndOfFile()
+  let output = String(data: data, encoding: .utf8)!
+
+  print("\(output)")
+
+  return output
+}
+
+func buildDPI(buildDir: String) {
+  /// Build DPI library
+  print("Building DPI")
+  let cargoFlags = ["build", "--release", "--features=sv2023"]
+  _ = try? createShell("cd fangshanemu && cargo \(cargoFlags.joined(separator: " "))")
+}
+
+func runCompile() {
+  /// Run mill compile and assembly
+  print("Running mill compile and assembly")
+  _ = try? createShell("./mill -i __.compile")
+}
+
+func runBuild(name: String, buildDir: String) {
+  /// Elaborate the design
+  let elaboratorFlags = [
+    "-cp", "out/elaborator/assembly.dest/out.jar",
+    "fangshan.elaborator.\(name)Main",
+    "design",
+    "--parameter",
+    "./configs/\(name).json",
+    "--target-dir",
+    ".",
+  ]
+  print("Running \(name) elaborator")
+  _ = try? createShell("java \(elaboratorFlags.joined(separator: String(" ")))")
+}
+
+func runEmit(name: String, emitDir: String) {
+  /// Run firtool to emit verilog, firrtl and mlir files
+  let firtoolFlags = [
+    "\(name).fir",
+    "--split-verilog",
+    "--annotation-file=\(name).anno.json",
+    "-o",
+    "\(emitDir)/emit",
+  ]
+  _ = try? createShell("firtool \(firtoolFlags.joined(separator: " "))")
+}
+
+func runEmu(name: String, buildDir: String) {
+  let emuDir = buildDir + "/emu"
+  buildDPI(buildDir: buildDir)  // Build DPI before running emu
+  /// Copy the DPI library to the build directory
+  let (_, _) = (
+    try? createShell("mkdir -p \(emuDir)"),
+    try? createShell("cp fangshanemu/target/debug/libfangshanemu.a \(emuDir)")
+  )
+
+  let verilatorFlags = [
+    "--trace-fst",
+    "--timing",
+    "--threads 1",
+    "-Mdir \(emuDir)",
+    "-O1",
+    "--main", "--exe",
+    "--cc",
+    "-I\(buildDir)/emit",
+    "-f \(buildDir)/emit/filelist.f",
+    "--top FangShanTestBench",
+    "libfangshanemu.a",
+  ]
+
+  print("Running emu")
+  _ = try? createShell("verilator \(verilatorFlags.joined(separator: " "))")
+  print("Building emu")
+  _ = try? createShell("cd \(emuDir) && make -j`nproc` -f V\(name).mk V\(name)")
+}
+
+do {
+  var (name, dirPath) = ("", "")
+  print("Enter emit target:")
+  print("1. FangShan\n")
+  print("2. FangShanTestbench\n")
+  print("Your choice:")
+  let target = readLine(strippingNewline: true)
+  switch target {
+  case "1":
+    name = "FangShan"
+    dirPath = "./fangshan-sim-result"
+    runCompile()
+    runBuild(name: name, buildDir: dirPath)
+    runEmit(name: name, emitDir: dirPath)
+  case "2":
+    name = "FangShanTestBench"
+    dirPath = "./fangshan-sim-result"
+    runBuild(name: name, buildDir: dirPath)
+    runEmit(name: name, emitDir: dirPath)
+    runEmu(name: name, buildDir: dirPath)
+  default:
+    print("Invalid target")
+    exit(1)
+  }
+}
