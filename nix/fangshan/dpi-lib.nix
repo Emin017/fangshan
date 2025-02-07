8c651da271b3f32dabfd92efeec5cb6640e42d1d
Qiming Chu <cchuqiming@gmail.com>
2025-02-07 16:04:32 +0800

    [nix] Update cargoHash in dpi-lib.nix
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- nix/fangshan/dpi-lib.nix
+++ nix/fangshan/dpi-lib.nix
@@ -14,7 +14,7 @@
 rustPlatform.buildRustPackage rec {
   name = "dpi-lib";
   src = ./../../${dpiLibName};
-  cargoHash = "sha256-/AEhkYQPGQ6PW4wmVCX7p9H30nuiG8Jr1oSyqhoWseY=";
+  cargoHash = "sha256-L910NAZxJcBOp6fIvkkG0Mxz3N+2HaZRqF//ZUaVi1g=";
   buildFeatures = lib.optionals sv2023 [ "sv2023" ]
     ++ lib.optionals vpi [ "vpi" ] ++ lib.optionals enable-trace [ "trace" ];
 
