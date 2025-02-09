c4eea9a13fdcb54529c21b8817462bb9e210e7eb
qiming chu <cchuqiming@gmail.com>
2025-02-09 18:00:44 +0800

    [nix] remove local packed add-determinism and use upstream version (#5)

--- nix/fangshan/dpi-lib.nix
+++ nix/fangshan/dpi-lib.nix
@@ -14,7 +14,7 @@
 rustPlatform.buildRustPackage rec {
   name = "dpi-lib";
   src = ./../../${dpiLibName};
-  cargoHash = "sha256-L910NAZxJcBOp6fIvkkG0Mxz3N+2HaZRqF//ZUaVi1g=";
+  cargoHash = "sha256-NTJwxjg8L8XwJMfpxp8GMVTLXHofCOPPYXMNEqNHeIs=";
   buildFeatures = lib.optionals sv2023 [ "sv2023" ]
     ++ lib.optionals vpi [ "vpi" ] ++ lib.optionals enable-trace [ "trace" ];
 
