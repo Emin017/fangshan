6598ac67cabf513e88bb84b0d164f658fac1ed5d
Qiming Chu <cchuqiming@gmail.com>
2025-11-21 23:38:37 +0800

    [test] refactor fs simulator in decoder tests
    * Replace FSVerilatorSimulator with FSSimulator object
    * Simplify test setup by removing controller wrapper and accessing DUT
    directly
    
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

---
+++ fangshan/test/src/simulator/FSSimulator.scala
@@ -0,0 +1,56 @@
+package fangshan.rtl.simulator
+
+import chisel3.simulator._
+import svsim._
+import java.nio.file.Files
+import java.io.File
+import scala.reflect.io.Directory
+
+object FSSimulator extends PeekPokeAPI {
+  trait SimulatorWithWorkspace {
+    def apply[T <: chisel3.RawModule](
+      module:       => T,
+      layerControl: LayerControl.Type = LayerControl.EnableAll
+    )(body:         T => Unit
+    ): Unit
+  }
+
+  def getSimulator(workspace: String): SimulatorWithWorkspace = {
+    new SimulatorWithWorkspace {
+      def apply[T <: chisel3.RawModule](
+        module:       => T,
+        layerControl: LayerControl.Type = LayerControl.EnableAll
+      )(body:         T => Unit
+      ): Unit = {
+        simulate(workspace, module, layerControl)(body)
+      }
+    }
+  }
+
+  def simulate[T <: chisel3.RawModule](
+    workspace:    String,
+    module:       => T,
+    layerControl: LayerControl.Type = LayerControl.EnableAll
+  )(body:         (T) => Unit
+  ): Unit = {
+    makeSimulator(workspace).simulate(module, layerControl)({ module => body(module.wrapped) }).result
+  }
+
+  private class DefaultVerilatorSimulator(val workspacePath: String) extends SingleBackendSimulator[verilator.Backend] {
+    val backend                            = verilator.Backend.initializeFromProcessEnvironment()
+    val tag                                = "verilator"
+    val commonCompilationSettings          = CommonCompilationSettings()
+    val backendSpecificCompilationSettings = verilator.Backend.CompilationSettings()
+
+    sys.addShutdownHook {
+      (new Directory(new File(workspacePath))).deleteRecursively()
+    }
+  }
+
+  private def makeSimulator(workspace: String): DefaultVerilatorSimulator = {
+    val id = ProcessHandle.current().pid().toString()
+    new DefaultVerilatorSimulator(
+      workspacePath = Files.createTempDirectory(s"${workspace}_${id}_").toString
+    )
+  }
+}
